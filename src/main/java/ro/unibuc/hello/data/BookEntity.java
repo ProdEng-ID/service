a7b5034f4a49323708d006fadfd32a9c1f262523
ioana-velniceru <ioana.velniceru@yahoo.com>
2023-03-11 18:33:40 +0200

    [API] Made properties of BookEntity private, fixed some bugs

--- src/main/java/ro/unibuc/hello/data/BookEntity.java
+++ src/main/java/ro/unibuc/hello/data/BookEntity.java
@@ -4,17 +4,49 @@ import org.springframework.data.annotation.Id;
 
 public class BookEntity {
     @Id
-    public String id;
+    private String id;
 
-    public String title;
-    public String author;
-    public String genre;
+    private String title;
+    private String author;
+    private String genre;
 
     public BookEntity() {}
 
     public BookEntity(String title, String author, String genre) {
         this.title = title;
         this.author = author;
+        this.genre = genre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
         this.genre = genre;
     }
 

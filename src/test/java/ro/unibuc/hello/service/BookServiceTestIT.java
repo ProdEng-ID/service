b121a426d4b1d89e425dc6a9e486a13b4395f9cf
Zaharia Diana Cristiana <dianacristiana2002@gmail.com>
2023-04-06 19:49:41 +0300

    Update BookServiceTestIT.java

--- src/test/java/ro/unibuc/hello/service/BookServiceTestIT.java
+++ src/test/java/ro/unibuc/hello/service/BookServiceTestIT.java
@@ -1,13 +1,16 @@
 package ro.unibuc.hello.service;
 
+import org.bson.types.ObjectId;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import ro.unibuc.hello.data.BookEntity;
 import ro.unibuc.hello.data.BookRepository;
 import ro.unibuc.hello.dto.BookDTO;
-
+import org.junit.jupiter.api.BeforeEach;
+import org.bson.types.ObjectId;
 @SpringBootTest
 @Tag("IT")
 public class BookServiceTestIT {
@@ -18,6 +21,37 @@ public class BookServiceTestIT {
     @Autowired
     BookService bookService;
 
+    BookEntity mybook;
+    String deleteresp;
+    String id;
+
+    String str;
+
+    @BeforeEach
+    void setUp() {
+        id = "2";
+        mybook = new BookEntity("Title2", "Author2","genre2");
+        mybook.setId(id);
+        deleteresp = "Book " + id + " has been successfully deleted.";
+    }
+
+    @Test
+    void insertUser() {
+        BookDTO result = bookService.createBook(mybook.getTitle(), mybook.getAuthor(), mybook.getGenre());
+
+        Assertions.assertEquals(result.getTitle(), "Title2");
+        Assertions.assertEquals(result.getAuthor(), "Author2");
+        Assertions.assertEquals(result.getGenre(), "genre2");
+    }
+
+    @Test
+    void deleteBook() {
+        bookRepository.save(mybook);
+        String result = bookService.deleteBook(id);
+        Assertions.assertEquals(result, deleteresp);
+    }
+
+
     @Test
     void getBook() {
         BookDTO book = bookService.getBook("6413456b37d7e23385bfe7f2");
@@ -26,4 +60,5 @@ public class BookServiceTestIT {
         Assertions.assertEquals(book.getAuthor(), "John Doe");
         Assertions.assertEquals(book.getGenre(), "Fantasy");
     }
+    
 }

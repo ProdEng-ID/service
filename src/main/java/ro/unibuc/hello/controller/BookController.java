a158031e123e3ea80761ff9fc49d9d28c19c73d2
ioana-velniceru <ioana.velniceru@yahoo.com>
2023-05-04 21:48:03 +0300

    [Monitoring] Monitoring setup, added some metrics

--- src/main/java/ro/unibuc/hello/controller/BookController.java
+++ src/main/java/ro/unibuc/hello/controller/BookController.java
@@ -1,5 +1,8 @@
 package ro.unibuc.hello.controller;
 
+import io.micrometer.core.annotation.Counted;
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +21,9 @@ public class BookController {
     @Autowired
     private BookService bookService;
 
+    @Autowired
+    MeterRegistry metricsRegistry;
+
     @PostMapping("/book/create")
     @ResponseBody
     public BookDTO createBook(@RequestParam(name="title") String title, @RequestParam(name="author") String author, @RequestParam(name="genre") String genre) {
@@ -25,6 +31,8 @@ public class BookController {
     }
     @GetMapping("/book/getAll")
     @ResponseBody
+    @Timed(value = "book.getAll.time", description = "Time taken to return all books")
+    @Counted(value = "book.getAll.count", description = "Number of times all books were returned")
     public List<BookDTO> getBooks() {
         return bookService.getBooks();
     }
@@ -43,6 +51,8 @@ public class BookController {
 
     @PutMapping("/book/edit")
     @ResponseBody
+    @Timed(value = "book.edit.time", description = "Time taken to edit a book")
+    @Counted(value = "book.edit.count", description = "Number of times books were edited")
     public BookDTO editBook(@RequestParam(name="id") String id, @RequestParam(name="title", required=false) String title, @RequestParam(name="author", required=false) String author, @RequestParam(name="genre", required=false) String genre) {
         return bookService.editBook(id, title, author, genre);
     }
